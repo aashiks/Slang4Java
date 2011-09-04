1a856df6100f9438419a914e7e8e435347a7e38f
aashiks <aashiks@aashiks.in>
2011-09-04 15:09:20 +0530

    Step 3 of SLANGFORDOTNET has been ported and PRINT, PRINTLINE (Statements ) work. The outputs of the mono and java version match.
    Difference from source : instead of handling control chars in the lexer's switch, they are skipped over in the code that checks for whitespace. 
    
     Also, goddamn Java does not have a goto statement or keyword.

--- CallSlang/src/callslang/CallSlang.java
+++ CallSlang/src/callslang/CallSlang.java
@@ -5,6 +5,7 @@
 package callslang;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import slang4java.*;
 
 /**
@@ -16,17 +17,38 @@ public class CallSlang {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
-        ExpressionBuilder b = new ExpressionBuilder("-2*((6+6)/3)");
-        Expression e = b.GetExpression();
-        //
-        // Evaluate the Expression
-        //
-        System.out.println(e.Evaluate(null));
+    static void TestFirstScript() throws Exception {
+        String a = "PRINTLINE 2*10;" + "\r\n" + "PRINTLINE 10;\r\n PRINT 2*10;\r\n";
+        RDParser p = new RDParser(a);
+        ArrayList arr = p.Parse();
+        for (Object obj : arr) {
+            Statement s = (Statement) obj;
+            s.Execute(null);
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
 
-        //
-        // Pause for a key stroke
-        //
+    static void TestSecondScript() throws Exception {
+        String a = "PRINTLINE -2*10;" + "\r\n" + "PRINTLINE -10*-1;\r\n PRINT 2*10;\r\n";
+        RDParser p = new RDParser(a);
+        ArrayList arr = p.Parse();
+        for (Object obj : arr) {
+            Statement s = (Statement) obj;
+            s.Execute(null);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            TestFirstScript();
+            TestSecondScript();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
         System.in.read();
     }
 }
